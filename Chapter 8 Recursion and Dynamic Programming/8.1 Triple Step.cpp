f01a652a860e6df3c67bd38f774ecec362886a44
Mukul Kumar <mukul.2121@gmail.com>
2020-06-25 11:13:22 +0530

    triple step memoisation

--- Chapter 8 Recursion and Dynamic Programming/8.1 Triple Step.cpp
+++ Chapter 8 Recursion and Dynamic Programming/8.1 Triple Step.cpp
@@ -5,7 +5,15 @@ Implement a method to count how many possible
 ways the child can run up the stairs.
 */
 #include<iostream>
+#include<vector>
 using namespace std;
+
+//function to display and observe dp array
+void display(vector<int> dp){
+    for(int i = 0 ; i < dp.size(); i++) {
+        cout << dp[i] << " ";
+    }cout << endl;
+}
 
 int tripleStepRecursive(int n) {
     if(n < 0){
@@ -16,8 +24,27 @@ int tripleStepRecursive(int n) {
     return tripleStepRecursive(n-1) + tripleStepRecursive(n-2) + tripleStepRecursive(n-3);
 }
 
+int tripleStepMemoisation(int n, vector<int>& dp) {
+    if(n < 0){
+        return  0;
+    }else if (n == 0){
+        return dp[n] = 1;
+    }
+
+    if(dp[n] != -1){
+        return dp[n];
+    }
+
+    int count = tripleStepMemoisation(n-1, dp) + tripleStepMemoisation(n-2, dp) + tripleStepMemoisation(n-3, dp);
+    return dp[n] = count;
+}
+
+
 int main () {
     int n;
     cin >> n;
+    vector<int> dp(n+1, -1);
     cout << tripleStepRecursive(n) << endl;
+    cout << tripleStepMemoisation(n, dp) << endl;
+    display(dp);
 }
