89f867a1c3db840f25c218a367ee09805f07ac0d
Mukul Kumar <mukul.2121@gmail.com>
2020-06-29 22:59:08 +0530

    robot in a grid DP

--- Chapter 8 Recursion and Dynamic Programming/8.2 Robot in a grid.cpp
+++ Chapter 8 Recursion and Dynamic Programming/8.2 Robot in a grid.cpp
@@ -52,6 +52,34 @@ int robotInGridMemoisation(vector<vector<int>>& input, int i, int j, vector<vect
 }
 
 
+int robotInGridDP(vector<vector<int>>& input, int i, int j, vector<vector<int>>& dp) {
+    int n = input.size(), m = input[0].size();
+    for(int i = n-1; i >= 0; i--){
+        for(int j = m-1; j >= 0; j--){
+            if(input[i][j] == 1){
+                dp[i][j] = 0;
+                continue;
+            }else if(i == n-1 && j == m-1){
+                dp[i][j] = 1;
+                continue;
+            }
+
+            int down = 0, right = 0;
+            if(i+1 <= n-1) {
+                down += dp[i+1][j];
+            }
+
+            if(j+1 <= m-1){
+                right += dp[i][j+1];
+            }
+            dp[i][j] = (right + down);
+        }
+    }
+    return dp[0][0];
+}
+ 
+
+
 int main () {
     vector<vector<int>> input = {{0, 0, 0},
                                 {0, 1, 0},
@@ -60,5 +88,7 @@ int main () {
 
     vector<vector<int>> dp(3, vector<int>(3, -1));
     cout << robotInGridRecursive(input, 0, 0) << endl;
-    cout << robotInGridMemoisation(input, 0, 0, dp) << endl;
+    // cout << robotInGridMemoisation(input, 0, 0, dp) << endl;
+    cout << robotInGridDP(input, 0, 0, dp) << endl;
+
 }
